6d1a0080dbdb3210d0aa7f04f094f02849ec7a9f
baculaa <baculaa@oregonstate.edu>
2022-03-17 16:02:39 -0700

    Added circle function

--- creative_code_lib/creative_code_lib.py
+++ creative_code_lib/creative_code_lib.py
@@ -15,18 +15,29 @@ class creative_code_lib:
           width=line_width,
           dash=dash_type))
 
-    def plot_rectangle(self,corner1_x,corner1_y,corner2_x,corner2_y,outline_color,outline_witdh,fill_color,fill_opacity):
+    def plot_rectangle(self,corner1_x,corner1_y,corner2_x,corner2_y,outline_color,outline_width,fill_color,fill_opacity):
         self.fig.add_shape(type="rect",
         x0=corner1_x, y0=corner1_y, x1=corner2_x, y1=corner2_y,
         line=dict(
           color=outline_color,
-          width=outline_witdh,
+          width=outline_width,
         ),
         fillcolor=fill_color,
         opacity=fill_opacity,)
 
-    def plot_circle(self,):
-        pass
+    def plot_circle(self,center_x,center_y,radius,outline_color,outline_width,fill_color,fill_opacity):
+        # Calculate plotly points based on center and radius
+        x0 = center_x - radius
+        y0 = center_y - radius
+        x1 = center_x + radius
+        y1 = center_y + radius
+        self.fig.add_shape(type="circle",
+        x0 = x0, y0 = y0, x1 = x1, y1 = y1,
+        opacity=fill_opacity,
+        fillcolor=fill_color,
+        line=dict(
+        color=outline_color,
+        width = outline_width))
 
 
 if __name__ == '__main__':
