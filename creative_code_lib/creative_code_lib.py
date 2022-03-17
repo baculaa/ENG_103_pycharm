c9476896e8dbb8bb20c995ce8f60ac6609083395
baculaa <baculaa@oregonstate.edu>
2022-03-17 14:28:06 -0700

    changed to using plotly for easier figure saving and plotting multiple things on one figure

--- creative_code_lib/creative_code_lib.py
+++ creative_code_lib/creative_code_lib.py
@@ -1,15 +1,20 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 class creative_code_lib:
     def __init__(self):
-        pass
-    def plot_circle(self,center_x,center_y,radius):
-        plt.axes()
-        circle = plt.Circle((center_x, center_y), radius, fc='y')
-        plt.gca().add_patch(circle)
-        plt.axis('scaled')
-        plt.show()
+        self.fig = go.Figure()
+
+    def plot_line_endpoints(self,start_x,start_y,end_x,end_y,line_color,line_width,dash_type):
+        self.fig.add_shape(type="line",
+        x0=start_x, y0=start_y, x1=end_x, y1=end_y,
+        line=dict(
+          color=line_color,
+          width=line_width,
+          dash=dash_type))
 
 if __name__ == '__main__':
     creative_code = creative_code_lib()
-    creative_code.plot_circle(0,0,5)
+    # TESTING FUNCTIONS #
+    creative_code.plot_line_endpoints(0,0,1,1,'blue',4,'dash')
+    creative_code.fig.update_shapes(dict(xref='x', yref='y'))
+    creative_code.fig.show()
