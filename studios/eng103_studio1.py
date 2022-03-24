76794d580d1b4ab81a0a5bf84932842f56129d31
baculaa <baculaa@oregonstate.edu>
2022-03-24 09:46:20 -0700

    Added studios folder

---
+++ studios/eng103_studio1.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # This function will print the following text and replace {name} with the value of the name variable
+    print(f'Hi, I am {name}')
+
+def print_animal(animal):
+    # This function will print the following text and replace {animal} with the value of the animal variable
+    print(f'My favorite animals are {animal}')
+
+def print_goals(my_goals):
+    # This function will print the following text and replace {my_goals} with the value of the my_goals variable
+    print(f'My goals for this class are {my_goals}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Change your variable values here, be sure to leave the quotation marks
+
+    # PUT YOUR NAME HERE
+    name = 'Alexandra'
+
+    # PUT YOUR FAVORITE ANIMAL HERE
+    animal = 'cats'
+
+    # PUT YOUR GOALS FOR THE CLASS HERE
+    my_goals = 'to help you all learn python and have fun creating cool visualizations and art along the way.'
+
+    # Call the functions to print your answers
+    print_hi(name)
+    print_animal(animal)
+    print_goals(my_goals)
